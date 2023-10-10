545dfed4fb2ed5f9041cc3c1a8054c4a0a61b7c6
a <128429841+VictorTI26@users.noreply.github.com>
2023-10-09 21:42:10 -0300

    1

---
+++ VITE-Desafio/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header/header';
+import Propaganda from './components/Propaganda/propaganda';
+
+const App = () => {
+  return <div>
+    <Header />
+    <Propaganda />
+  </div>;
+
+};
+
+export default App;
---
+++ VITE-Desafio/vite-project/src/components/Propaganda/propaganda.jsx
@@ -0,0 +1,11 @@
+import "./propaganda.css";
+
+function propaganda() {
+    return (
+        <div className="propaganda">
+            <img src="./public/images/Banner.jpg" alt="Banner" />
+        </div>
+    );
+    }
+
+export default propaganda;
