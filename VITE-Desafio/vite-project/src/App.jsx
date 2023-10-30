db9080c2b88f9c8f8a832da5a6f5f96822ecb97c
VictorTI26 <victor_micheluzzi@estudante.sesisenai.org.br>
2023-10-30 16:14:01 -0300

    2

--- VITE-Desafio/vite-project/src/App.jsx
+++ VITE-Desafio/vite-project/src/App.jsx
@@ -2,13 +2,15 @@ import Header from './components/Header/header';
 import Propaganda from './components/Propaganda/propaganda';
 import Categoria from './components/Categoria/categoria';
 import Produto from './components/Produto/produto';
+import Inspiration from './components/Inspiration/inspiration'
 
 const App = () => {
   return <div>
-    <Header />
-    <Propaganda />
+    <Header/>
+    <Propaganda/>
     <Categoria/>
     <Produto/>
+    <Inspiration/>
   </div>;
 
 };
--- VITE-Desafio/vite-project/src/components/Produto/produto.jsx
+++ VITE-Desafio/vite-project/src/components/Produto/produto.jsx
@@ -1,99 +1,94 @@
 import "./produto.css";
-function produto() {
-  return (
-    <div className="produto">
-      <p className="titulo-principal">Our Products</p>
+import PropTypes from "prop-types";
 
-      <div className="row">
-        <div className="singular">
-          <img src="./public/images/Syltherine.png" alt="Logo" />
-          <div className="espaco">
-            <p className="titulo">Syltherine</p>
-            <p className="texto">Stylish cafe chair</p>
-            <div className="preco">
-              <p className="titulo-preco">Rp 2.500.000</p>
-              <p className="texto-preco">Rp 3.500.000</p>
-            </div>
-          </div>
-        </div>
-        <div className="singular">
-          <img src="./public/images/Leviosa.png" alt="Logo" />
-          <div className="espaco">
-            <p className="titulo">Leviosa</p>
-            <p className="texto">Stylish cafe chair</p>
-            <div className="preco">
-              <p className="titulo-preco">Rp 2.500.000</p>
-            </div>
-          </div>
-        </div>
-        <div className="singular">
-          <img src="./public/images/Lolito.png" alt="Logo" />
-          <div className="espaco">
-            <p className="titulo">Lolito</p>
-            <p className="texto">Luxury big sofa</p>
-            <div className="preco">
-              <p className="titulo-preco">Rp 7.000.000</p>
-              <p className="texto-preco">Rp 14.000.000</p>
-            </div>
-          </div>
-        </div>
-        <div className="singular">
-          <img src="./public/images/Respira.png" alt="Logo" />
-          <div className="espaco">
-            <p className="titulo">Respira</p>
-            <p className="texto">Outdoor bar table and stool</p>
-            <div className="preco">
-              <p className="titulo-preco">Rp 500.000</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* separação */}
-      <div className="row2">
-        <div className="singular">
-          <img src="./public/images/Grifo.png" alt="Logo" />
-          <div className="espaco">
-            <p className="titulo">Grifo</p>
-            <p className="texto">Night lamp</p>
-            <div className="preco">
-              <p className="titulo-preco">Rp 1.500.000</p>
-            </div>
-          </div>
-        </div>
-        <div className="singular">
-          <img src="./public/images/Muggo.png" alt="Logo" />
-          <div className="espaco">
-            <p className="titulo">Muggo</p>
-            <p className="texto">Small mug</p>
-            <div className="preco">
-              <p className="titulo-preco">Rp 150.000</p>
-            </div>
-          </div>
-        </div>
-        <div className="singular">
-          <img src="./public/images/Pingky.png" alt="Logo" />
-          <div className="espaco">
-            <p className="titulo">Pingky</p>
-            <p className="texto">Cute bed set</p>
-            <div className="preco">
-              <p className="titulo-preco">Rp 7.000.000</p>
-              <p className="texto-preco">Rp 14.000.000</p>
-            </div>
-          </div>
-        </div>
-        <div className="singular">
-          <img src="./public/images/Potty.png" alt="Logo" />
-          <div className="espaco">
-            <p className="titulo">Potty</p>
-            <p className="texto">Minimalist flower pot</p>
-            <div className="preco">
-              <p className="titulo-preco">Rp 500.000</p>
-            </div>
-          </div>
+function ProdutoCard({ nome, descricao, precoAntes, precoAgora, imagemAlt }) {
+  return (
+    <div className="singular">
+      <img src={`./public/images/${imagemAlt}.png`} alt={imagemAlt} />
+      <div className="espaco">
+        <p className="titulo">{nome}</p>
+        <p className="texto">{descricao}</p>
+        <div className="preco">
+          <p className="titulo-preco">Rp {precoAgora}</p>
+          {precoAntes && <p className="texto-preco">Rp {precoAntes}</p>}
         </div>
       </div>
     </div>
   );
 }
 
-export default produto;
+ProdutoCard.propTypes = {
+  nome: PropTypes.string.isRequired,
+  descricao: PropTypes.string.isRequired,
+  precoAntes: PropTypes.string,
+  precoAgora: PropTypes.string.isRequired,
+  imagemAlt: PropTypes.string.isRequired,
+};
+
+function Produto() {
+  return (
+    <div className="produto">
+      <p className="titulo-principal">Our Products</p>
+
+      <div className="row">
+        <ProdutoCard
+          nome="Syltherine"
+          descricao="Stylish cafe chair"
+          precoAntes="3.500.000"
+          precoAgora="2.500.000"
+          imagemAlt="Syltherine"
+        />
+        <ProdutoCard
+          nome="Leviosa"
+          descricao="Stylish cafe chair"
+          precoAgora="2.500.000"
+          imagemAlt="Leviosa"
+        />
+        <ProdutoCard
+          nome="Lolito"
+          descricao="Luxury big sofa"
+          precoAntes="14.000.000"
+          precoAgora="7.000.000"
+          imagemAlt="Lolito"
+        />
+        <ProdutoCard
+          nome="Respira"
+          descricao="Outdoor bar table and stool"
+          precoAgora="500.000"
+          imagemAlt="Respira"
+        />
+      </div>
+
+      <div className="row2">
+        <ProdutoCard
+          nome="Grifo"
+          descricao="Night lamp"
+          precoAgora="1.500.000"
+          imagemAlt="Grifo"
+        />
+        <ProdutoCard
+          nome="Muggo"
+          descricao="Small mug"
+          precoAgora="150.000"
+          imagemAlt="Muggo"
+        />
+        <ProdutoCard
+          nome="Pingky"
+          descricao="Cute bed set"
+          precoAntes="14.000.000"
+          precoAgora="7.000.000"
+          imagemAlt="Pingky"
+        />
+        <ProdutoCard
+          nome="Potty"
+          descricao="Minimalist flower pot"
+          precoAgora="500.000"
+          imagemAlt="Potty"
+        />
+      </div>
+        <button className="botaoVerMais">Show More</button>
+    </div>
+  );
+}
+
+export default Produto;
