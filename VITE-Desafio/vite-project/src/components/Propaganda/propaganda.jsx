98ee53377a0f67418b6ff3c51ec38f1f3b2bdaf2
VictorTI26 <victor_micheluzzi@estudante.sesisenai.org.br>
2023-10-30 21:51:07 -0300

    5

--- VITE-Desafio/vite-project/src/components/Propaganda/propaganda.jsx
+++ VITE-Desafio/vite-project/src/components/Propaganda/propaganda.jsx
@@ -4,11 +4,11 @@ function propaganda() {
     return (
         <div className="propaganda">
             <img src="./public/images/Banner.jpg" alt="Banner" />
-            <div className="card">
-                <p>New Arrival</p>
-                <p>Discover Our</p>
-                <p>New Collection</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+            <div className="cardPropaganda">
+                <p className="textoPropaganda">New Arrival</p>
+                <p className="tituloPropaganda">Discover Our New Collection</p>
+                {/* <p className="tituloPropaganda"></p> */}
+                <p className="textoPropaganda">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
                 <button>BUY NOW</button>
             </div>
             <div className="browse">
